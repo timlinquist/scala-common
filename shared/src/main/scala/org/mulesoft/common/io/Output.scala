5e461fdaf78eaf29aab5d94cf7c6d4783754f937
Emilio Lopez Gabeiras <elopezgabeiras@salesforce.com>
2018-11-01 15:49:07 -0300

    String by CharSequence in Output

--- shared/src/main/scala/org/mulesoft/common/io/Output.scala
+++ shared/src/main/scala/org/mulesoft/common/io/Output.scala
@@ -10,7 +10,7 @@ import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
   *
   */
 trait Output[W] {
-  def append(w: W, s: String): Unit
+  def append(w: W, s: CharSequence): Unit
   def append(w: W, c: Char): Unit = append(w, c.toString)
 
   def write(w: W, cbuf: Array[Char], off: Int, len: Int): Unit = append(w, new String(cbuf, off, len))
@@ -25,8 +25,8 @@ object Output {
   /** OutputOps implements operations on W:Output mostly by delegation to the Output[W] trait */
   implicit class OutputOps[W](val w: W) {
 
-    def append(s: String)(implicit o: Output[W]): Unit = o.append(w, s)
-    def append(c: Char)(implicit o: Output[W]): Unit   = o.append(w, c)
+    def append(s: CharSequence)(implicit o: Output[W]): Unit = o.append(w, s)
+    def append(c: Char)(implicit o: Output[W]): Unit         = o.append(w, c)
 
     def write(w: W, cbuf: Array[Char], off: Int, len: Int)(implicit o: Output[W]): Unit = o.write(w, cbuf, off, len)
     def write(w: W, cbuf: Array[Char])(implicit o: Output[W]): Unit                     = o.write(w, cbuf)
@@ -37,8 +37,8 @@ object Output {
 
   implicit object OutputWriter extends Output[Writer] {
 
-    override def append(writer: Writer, string: String): Unit = writer.append(string)
-    override def append(writer: Writer, chr: Char): Unit      = writer.append(chr)
+    override def append(writer: Writer, string: CharSequence): Unit = writer.append(string)
+    override def append(writer: Writer, chr: Char): Unit            = writer.append(chr)
 
     override def write(w: Writer, cbuf: Array[Char], off: Int, len: Int): Unit = w.write(cbuf, off, len)
     override def write(w: Writer, cbuf: Array[Char]): Unit                     = w.write(cbuf)
