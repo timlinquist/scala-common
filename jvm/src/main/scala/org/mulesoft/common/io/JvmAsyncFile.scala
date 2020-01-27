caf6e6869b31b1d69adb66759d8ed45f8235ca88
Alejandro Loose <alejandro.loose@mulesoft.com>
2020-01-27 11:07:30 -0300

    APIMF-1829: added parameterized ExecutionContext for async file methods

--- jvm/src/main/scala/org/mulesoft/common/io/JvmAsyncFile.scala
+++ jvm/src/main/scala/org/mulesoft/common/io/JvmAsyncFile.scala
@@ -1,7 +1,6 @@
 package org.mulesoft.common.io
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Implementation of a AsyncFile for the JVM
@@ -16,12 +15,14 @@ protected class JvmAsyncFile(private val syncFile: JvmSyncFile) extends AsyncFil
   override def parent: String = syncFile.parent
   override def name: String   = syncFile.name
 
-  override def delete: Future[Unit]                                      = Future.successful(syncFile.delete)
-  override def list: Future[Array[String]]                               = Future.successful(syncFile.list)
-  override def mkdir: Future[Unit]                                       = Future(syncFile.mkdir)
-  override def read(encoding: String): Future[CharSequence]              = Future(syncFile.read(encoding))
-  override def write(data: CharSequence, encoding: String): Future[Unit] = Future(syncFile.write(data, encoding))
-  override def exists: Future[Boolean]                                   = Future(syncFile.exists)
-  override def isDirectory: Future[Boolean]                              = Future(syncFile.isDirectory)
-  override def isFile: Future[Boolean]                                   = Future(syncFile.isFile)
+  override def delete: Future[Unit]                                         = Future.successful(syncFile.delete)
+  override def list: Future[Array[String]]                                  = Future.successful(syncFile.list)
+  override def mkdir(implicit ctx: ExecutionContext = global): Future[Unit] = Future(syncFile.mkdir)
+  override def read(encoding: String)(implicit ctx: ExecutionContext = global): Future[CharSequence] =
+    Future(syncFile.read(encoding))
+  override def write(data: CharSequence, encoding: String)(implicit ctx: ExecutionContext = global): Future[Unit] =
+    Future(syncFile.write(data, encoding))
+  override def exists(implicit ctx: ExecutionContext = global): Future[Boolean]      = Future(syncFile.exists)
+  override def isDirectory(implicit ctx: ExecutionContext = global): Future[Boolean] = Future(syncFile.isDirectory)
+  override def isFile(implicit ctx: ExecutionContext = global): Future[Boolean]      = Future(syncFile.isFile)
 }
--- jvm/src/main/scala/org/mulesoft/common/io/JvmSyncFile.scala
+++ jvm/src/main/scala/org/mulesoft/common/io/JvmSyncFile.scala
@@ -2,6 +2,8 @@ package org.mulesoft.common.io
 
 import java.io.{FileInputStream, FileOutputStream, InputStreamReader, OutputStreamWriter, File => JFile}
 
+import scala.concurrent.ExecutionContext
+
 /**
   * Implementation of a SyncFile for the JVM
   */
@@ -14,11 +16,11 @@ protected class JvmSyncFile(val fileSystem: FileSystem, val path: String) extend
   override def parent: String      = file.getParent
   override def name: String        = file.getName
   override def list: Array[String] = file.list()
+  override def delete: Unit        = file.delete()
 
-  override def mkdir: Unit = file.mkdirs()
-  override def delete: Unit = file.delete()
+  override def mkdir(implicit ctx: ExecutionContext = global): Unit = file.mkdirs()
 
-  override def read(encoding: String): CharSequence = {
+  override def read(encoding: String)(implicit ctx: ExecutionContext = global): CharSequence = {
     val fis  = new InputStreamReader(new FileInputStream(file), encoding)
     val data = new Array[Char](file.length.toInt)
     val n    = fis.read(data)
@@ -26,13 +28,13 @@ protected class JvmSyncFile(val fileSystem: FileSystem, val path: String) extend
     if (n == data.length) data else data.subSequence(0, n)
   }
 
-  override def write(data: CharSequence, encoding: String): Unit = {
+  override def write(data: CharSequence, encoding: String)(implicit ctx: ExecutionContext = global): Unit = {
     val fos = new OutputStreamWriter(new FileOutputStream(file), encoding)
     fos.write(data.toString)
     fos.close()
   }
 
-  override def exists: Boolean      = file.exists()
-  override def isDirectory: Boolean = file.isDirectory
-  override def isFile: Boolean      = file.isFile
+  override def exists(implicit ctx: ExecutionContext = global): Boolean      = file.exists()
+  override def isDirectory(implicit ctx: ExecutionContext = global): Boolean = file.isDirectory
+  override def isFile(implicit ctx: ExecutionContext = global): Boolean      = file.isFile
 }
